64f9b735a1cf53c0e646e859f6eb35788da72397
sumr4693 <subramanian4693@gmail.com>
2025-07-06 21:19:25 +0200

    Added comments for k-largest

--- dsa/k_elements/k_largest.cpp
+++ dsa/k_elements/k_largest.cpp
@@ -10,11 +10,16 @@ int k_largest::find_k_largest()
 {
     size_t n = list_.size();
 
+    // Build a min-heap of the first k elements
     for (auto i = (k_count_ / 2) - 1; i >= 0; i--)
     {
         util::heap::min_heapify(list_, k_count_, i);
     }
 
+    // For the remaining elements, if the element is larger than the root of the heap,
+    // replace the root with the new element and re-heapify, so that the smallest of the k is again at the root.
+    // At the end, the heap contains the k largest elements,
+    // and the root is the smallest among them, which is the k-th largest element in the original list.
     for (size_t i = k_count_; i < n; i++)
     {
         if (list_[i] > list_[0])
